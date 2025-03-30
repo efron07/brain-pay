cdbfa987729b7a1b317c70595da7378257865933
Efron Damasco <efrondamasco@gmail.com>
2025-03-30 21:01:43 +0300

    UPDATE PACKAGE.JSON

--- src/assets/components/Cta.jsx
+++ src/assets/components/Cta.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { motion } from "motion/react"
 
 const Cta = () => {
     return (
         <div>
             <section className="bg-white dark:bg-blue-900 py-9">
                 <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-                    <img className="w-full hidden md:block " src=" assets/img/one.svg" alt="dashboard image" />
+                    <motion.img 
+                    className="w-full hidden md:block " src=" assets/img/one.svg" alt="dashboard image" />
                     <div className="mt-4 md:mt-0">
                         <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Let's create more tools and ideas that brings us together.</h2>
                         <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Brain Pay is a next-generation payment gateway designed to help businesses accept payments seamlessly, whether you're a startup, freelancer, e-commerce store, or enterprise. Our mission is to provide fast, secure, and affordable payment solutions that empower businesses to grow.</p>
--- src/assets/components/Hero.jsx
+++ src/assets/components/Hero.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import { motion } from "motion/react"
 
 const Hero = () => {
+
+    const headingVariants = {
+        hidden: { opacity: 0, y: 50 }, // Initial state for each letter
+        visible: (i) => ({
+            opacity: 1,
+            y: 0,
+            transition: {
+                delay: i * 0.05, // Delay each letter by 0.05s
+            },
+        }),
+    };
+
+    const headingText = "Seamless Payments. Smarter Transactions. Faster Growth.";
+
     return (
         <div>
             <section className=" bg-blue-850 py-10">
@@ -8,7 +23,22 @@ const Hero = () => {
                     {/* Left Content */}
                     <div className="flex flex-col items-start text-center  lg:text-left lg:w-7/12">
                         <h1 className="text-4xl text-left font-extrabold tracking-tight leading-tight mb-4 md:text-5xl xl:text-6xl dark:text-white">
-                            Seamless Payments. Smarter Transactions. Faster Growth.
+                            {headingText.split("").map((letter, index) => (
+                                <motion.span
+                                    key={index}
+                                    custom={index} 
+                                    variants={headingVariants}
+                                    initial="hidden"
+                                    animate="visible"
+                                    transition={{
+                                        duration: 0.3,
+                                        ease: "easeInOut",
+                                    }}
+                                    style={{ display: "inline-block" }}
+                                >
+                                    {letter === " " ? "\u00A0" : letter} 
+                                </motion.span>
+                            ))}
                         </h1>
                         <p className="text-gray-500 font-light mb-6 md:text-lg lg:text-xl dark:text-gray-400 text-justify ">
                             Accept payments globally with Brain Pay, the next-gen payment gateway designed for businesses of all sizes
@@ -31,7 +61,13 @@ const Hero = () => {
 
                 {/* Right Image */}
                 <div className="mt-8 lg:mt-0 lg:w-5/12 flex justify-center sm:hidden lg:block md:block">
-                    <img
+                    <motion.img
+                        initial={{ opacity: 0, scale: 1 }}  
+                        whileHover={{ scale:0.9 }}
+                        animate={{ opacity: 1, scale: 1 }}  
+                        transition={{ duration: 1,
+                        ease: "easeInOut" 
+                        }}     
                         src="assets/img/three.svg"
                         alt="nice image "
                         className="w-full h-auto hidden lg:block md:block"
